1d15a671a27a5e1ce083a287a6b4c272e5f20e9a
foorhad <forhadkhan20330@gmail.com>
2023-09-24 21:49:55 +0600

    p

---
+++ join  two set.py
@@ -0,0 +1,6 @@
+set1 = {"a", "b" , "c"}
+set2 = {11,22,55}
+
+set3 =set1.union(set2)
+print(set3)
+print(type(set3))
---
+++ search.py
@@ -0,0 +1,8 @@
+set= {55,22,11,555,88,}
+
+value =(input("Enter the exist value:  "))
+
+if value in set:
+    print('Value is founnd')
+else:
+    print("Value isn't found")
